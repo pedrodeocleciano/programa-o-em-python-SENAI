d429f6729097e82f29b7105b66bcea48e4cbc8b7
pedrodeocleciano <pedro842199@gmail.com>
2026-07-21 21:41:37 -0300

    Add files via upload

---
+++ Aula-4/Atividade aula 4.py
@@ -0,0 +1,26 @@
+#Atividade 1 
+numero = int(input('digite um numero: '))
+print (numero ** numero)
+
+#Atividade 2
+nome = input('digite seu nome:' )
+sobrenome = input('digite seu sobrenome:' )
+print(nome, sobrenome)
+
+#Atividade 3
+
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+print(str(n1), str(n2))
+
+#Atividade 4
+
+p = "python"
+n = input('digite um numero: ')
+print(p, n)
+
+#Atividade 5
+
+var1 = 'bom dia'
+var2 = input("digite uma palavra: ")
+print(var1, var2)
