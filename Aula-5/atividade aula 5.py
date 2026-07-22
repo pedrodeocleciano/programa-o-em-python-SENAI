ed53286630775d6e73c73b754cffe55a22b80e4c
pedrodeocleciano <pedro842199@gmail.com>
2026-07-22 20:11:02 -0300

    Add files via upload

---
+++ Aula-5/atividade aula 5.py
@@ -0,0 +1,46 @@
+#1
+
+print('boas-vindas')
+
+#2 
+verdadeira = True
+print(type(verdadeira))
+
+
+#3
+
+mult = 2.50 * 5.50
+print(mult)
+
+#4 
+
+div = 6 / 2
+print(div)
+
+#5 
+sub = 5 - 3
+print(sub)
+
+#6
+
+div2 = 8//2
+print(div2)
+
+#7
+
+mult2 = 2 * 2 * 3 * 4
+print(mult2)
+
+#8
+
+numero = 12
+dobro = 12 * 2
+print(dobro)
+
+#9
+
+nome = input('digite seu nome: ')
+email = input('digite seu email: ')
+idade = input('digite sua idade: ')
+graduacao = input('qual sua graduacao: ')
+estado = input('seu estado civil é: ')
