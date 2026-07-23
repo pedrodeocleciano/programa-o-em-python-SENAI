bbe3f5f873fbd3aaa5fb05015af0c62f89faa2e5
pedrodeocleciano <pedro842199@gmail.com>
2026-07-23 19:35:05 -0300

    Add files via upload

---
+++ Aula-6/aula 6.py
@@ -0,0 +1,53 @@
+#1
+
+modulo = 17 % 4
+print(modulo)
+
+#2
+
+mult = (9 * 3) ** 2
+print(mult)
+
+#3
+
+numero = 81
+raiz = numero ** (1/2)
+print(raiz)
+
+#4
+
+ad = (3 * 4) + 20
+print (ad)
+
+#5
+
+mult2 = (15 * 2) - 7 
+print(mult2)
+
+#6
+
+cubo= 5 ** 3
+print(cubo)
+
+#7
+
+n1 = 17
+n2 = 21
+n3 = 25
+media = (n1 + n2 + n3) / 3 
+print(media)
+
+#8
+
+mult3 =(11*2)+ 7 
+print(mult3)
+
+#9
+
+sub = ((3 *8) - 15) / 2
+print(sub)
+
+#10
+
+p = 2 ** 10
+print(p)
