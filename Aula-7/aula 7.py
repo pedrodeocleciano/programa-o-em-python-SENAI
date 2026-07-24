b1ac9e3a1bb6f0c61678aca3e3357cd55bb76742
pedrodeocleciano <pedro842199@gmail.com>
2026-07-23 21:23:47 -0300

    Add files via upload

---
+++ Aula-7/aula 7.py
@@ -0,0 +1,25 @@
+#0
+lista = list(range(2,21,2))
+print(lista)
+#1
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numeros)
+
+#2
+
+print(numeros[2])
+
+#3
+
+numeros.append (9)
+print(numeros)
+
+#4
+
+numeros.remove(5)
+print(numeros)
+
+#5
+carros = ["sentra", "corsa", "civic"]
+print(carros, numeros)
