a65ca65952cc9e6de4cc05b6ddb5ef5a9d0cad83
pedrodeocleciano <pedro842199@gmail.com>
2026-07-29 21:53:39 -0300

    Add files via upload

---
+++ Aula-11/aula 11.py
@@ -0,0 +1,14 @@
+try:
+    numero = int(input("digite um numero inteiro: "))
+    print('voce digitou: ', numero)
+except ValueError: 
+    print('erro! digite apenas um numero inteiro.')
+
+#2
+
+try:
+    num1 = float(input)('digite o primeiro numero: ')
+    num2 = float(input)('digite o segundo numero: ')
+    
+
+
